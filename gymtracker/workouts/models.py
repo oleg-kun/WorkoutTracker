6ffd5aba7e7a227521aa2c5221d714d5165b151f
Oleg Kun <oleg.gamayun@gmail.com>
2025-03-07 07:45:19 +0300

    Superuser added

--- gymtracker/workouts/models.py
+++ gymtracker/workouts/models.py
@@ -14,3 +14,16 @@ class Workout(models.Model):
     )
     date = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"This is workout of {self.name} ({self.date})"
+class Exercise(models.Model):
+    workout_connection = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name="exercises") #привязка к тренировке
+    name = models.CharField(max_length=50)
+    sets = models.PositiveIntegerField()
+    reps = models.PositiveIntegerField()
+    weight = models.FloatField()
+
+    def __str__(self):
+        return f"This is exercise {self.name}, {self.sets} x {self.reps} {self.weight}kg"
+
